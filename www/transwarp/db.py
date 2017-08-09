bc350bd64bd075acba234b305fec0a7da2c85278
gaohan <34242696@qq.com>
2017-08-09 22:12:22 +0800

    add some codes

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -18,9 +18,9 @@ import time, uuid, function, threading, logging
 
 
 class Dict(dict):
-    def __init_(self, names=(), values(), **kw):
+    def __init__(self, names=(), values=(), **kw):
         super(Dict, self).__init__(**kw)
-        for k, v int zip(names, values):
+        for k, v in zip(names, values):
             self[k] = v
     
     
@@ -33,3 +33,121 @@ class Dict(dict):
     
     def __setattr__(self, key, value):
         self[key] = value
+            
+            
+def next_id(t = None):
+    if t is None:
+        t = time.time()
+    return '%015d%s000' % (int(t * 1000), uuid.uuid4().hex)
+
+def _profiling(start, sql = ''):
+    t = time.time() - start
+    if t > 0.1:
+        logging.waring('[PROFILING][DB] %s: %s' % (t, sql))
+    else:
+        logging.info('[PROFILING][DB] %s: %s' % (t, sql))
+
+
+class DBError(Exception):
+    pass
+
+
+class MultiColumsError(DBError):
+    pass
+
+
+class _LasyConnection(object):
+    
+    
+    def __init__(self):
+        if self.connection is None:
+            connection = engine.connect()
+            logging.info('open connection <%s>...' % hex(id(connection)))
+            self.connection = connection
+        return self.conncetion.cursor()
+    
+
+def commit(self):
+    self.connection.commit()
+    
+
+def rollback(self):
+    self.connection.rollback()
+    
+
+def cleanup(self):
+    if self.connection:
+        connection = self.connection
+        self.connection = None
+        logging.info('close connection <%s>...' % hex(id(connection)))
+        connection.close()
+
+
+class _DbCtx(threading.local):
+    def __init__(self):
+        self.connection = None
+        self.transactions = 0
+    
+    
+    def is_init(self):
+        return not self.connection is None
+    
+    
+    def init(self):
+        logging.info('open lazy connection...')
+        self.connection = _LasyConnection()
+        self.transactions = 0
+        
+    
+    def cleanup(self):
+        self.connection.cleanup()
+        self.conncetion = None
+    
+    
+    def cursor(self):
+        return self.connection.cursor()
+    
+_db_ctx = _DbCtx()
+
+#global engine object:
+engine = None
+
+
+class _Engine(object):
+    def __init__(self, connect):
+        self.connect = connect
+     
+        
+    def connect(self):
+        return self._connect()
+    
+
+def create_engine(user, password, database, host = '127.0.0.1', port = 3306, **kw):
+    import mysql.connector
+    global engine
+    if engine is not None:
+        raise DBError('Engine is already initialized.')
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
