8ad7b50abdeae7fe9297858ad7aef67f9c0d95c9
gaohan <34242696@qq.com>
2017-08-10 14:30:03 +0800

    contiune coding

---
+++ www/transwarp/orm.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 10 14:27:07 2017
+
+@author: Michael Gao
+"""
+
+__author__ = 'Michael Gao'
+
+
+'''
+Database operation module. This module is independent with web module.
+'''
+
+import time, logging
+import db
